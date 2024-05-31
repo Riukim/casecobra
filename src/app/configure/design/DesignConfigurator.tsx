9f301a69dda2055651d7b8cb61708fdce2fa1154
Riukim <m.cominelli29@gmail.com>
2024-05-31 16:17:01 +0200

    Implement image drag and drop and resizing, start color configurator radio group

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -1,6 +1,15 @@
+"use client"
+
+import HandleComponent from "@/components/HandleComponent"
 import { AspectRatio } from "@/components/ui/aspect-ratio"
+import { ScrollArea } from "@/components/ui/scroll-area"
 import { cn } from "@/lib/utils"
 import NextImage from "next/image"
+import { Rnd } from "react-rnd"
+import { Radio, RadioGroup } from "@headlessui/react"
+import { useState } from "react"
+import { COLORS } from "@/validators/option-validator"
+import { Label } from "@/components/ui/label"
 
 interface DesignConfiguratorProps {
   configId: string
@@ -13,6 +22,13 @@ const DesignConfigurator = ({
   imgUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+
+  const [options, setOptions] = useState<{
+    color: (typeof COLORS)[number]
+  }>({
+    color: COLORS[0]
+  })
+
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
@@ -31,10 +47,91 @@ const DesignConfigurator = ({
           <div className="absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]" />
           <div
             className={cn(
-              "absolute inset-0 left-[3px] right-[3px] top-px bottom-px rounded-[32px]" , `bg-blue-950` // temporary mock value
+              "absolute inset-0 left-[3px] right-[3px] top-px bottom-px rounded-[32px]",
+              `bg-zinc-950` // temporary mock value
             )}
           />
         </div>
+
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+          lockAspectRatio
+          className="absolute z-20 border-[3px] border-primary"
+          resizeHandleComponent={{
+            bottomRight: <HandleComponent />,
+            bottomLeft: <HandleComponent />,
+            topLeft: <HandleComponent />,
+            topRight: <HandleComponent />,
+          }}
+        >
+          <div className="relative w-full h-full">
+            <NextImage
+              src={imgUrl}
+              fill
+              alt="Your image"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
+      </div>
+
+      <div className="h-[37.rem] flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              <RadioGroup
+                value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({
+                    ...prev,
+                    color: val,
+                  }))
+                }}
+              >
+                <Label>Color: {options.color.label}</Label>
+                <div className="mt-3 flex items-center space-x-3">
+                  {COLORS.map((color) => (
+                    <RadioGroup.Option
+                      key={color.label}
+                      value={color}
+                      className={({ active, checked }) =>
+                        cn(
+                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                          {
+                            [`border-${color.tw}`]: active || checked,
+                          }
+                        )
+                      }
+                    >
+                      <span
+                        className={cn(
+                          `bg-${color.tw}`,
+                          "h-8 w-8 rounded-full border border-black border-opacity-10"
+                        )}
+                      />
+                    </RadioGroup.Option>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   )
