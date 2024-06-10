7736282a74d344ae25edb396902021fcada81a3b
Riukim <m.cominelli29@gmail.com>
2024-06-10 10:34:52 +0200

    change resend email

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       })
 
       await resend.emails.send({
-        from: "CaseCobra <onboarding@resend.dev>",
+        from: "CaseCobra <m.cominelli29@gmail.com>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
